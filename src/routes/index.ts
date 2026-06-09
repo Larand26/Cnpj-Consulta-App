26ab506e25864e91f93460cdc8519b6992cf0ec2
vbl09012003vbl@gmail.com <vbl09012003vbl@gmail.com>
2026-06-09 08:20:40 -0300

    feat: adiciona rota para index.html e index.css

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 
 const routes = Router();
 
 import CnpjController from "../controllers/CnpjController.js";
 
 routes.get("/", (req: Request, res: Response) => {
-  res.send("Hello from the routes!");
+  const html = fs.readFileSync(
+    path.join(process.cwd(), "public/index.html"),
+    "utf-8",
+  );
+  res.send(html);
 });
 
 routes.post("/cnpj-consult", async (req: Request, res: Response) => {
