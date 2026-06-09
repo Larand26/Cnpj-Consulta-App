ed8b288b2a9f05821519fb811a55162e4577bb08
vbl09012003vbl@gmail.com <vbl09012003vbl@gmail.com>
2026-06-09 08:51:26 -0300

    feat: importa a pasta public para o servidor

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,20 @@
 import express from "express";
+import path from "path"; // Importe o path
 import routes from "./routes/index.js";
 
 class App {
   server: express.Express;
+
   constructor() {
     this.server = express();
+    this.middlewares(); // Chame os middlewares antes das rotas!
     this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.static(path.join(process.cwd(), "public")));
+    this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: true }));
   }
 
   routes() {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import fs from "fs";
 import path from "path";
 
 const routes = Router();
@@ -8,11 +7,7 @@ const routes = Router();
 import CnpjController from "../controllers/CnpjController.js";
 
 routes.get("/", (req: Request, res: Response) => {
-  const html = fs.readFileSync(
-    path.join(process.cwd(), "public/index.html"),
-    "utf-8",
-  );
-  res.send(html);
+  res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
 routes.post("/cnpj-consult", async (req: Request, res: Response) => {
